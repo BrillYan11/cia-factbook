8ea7a6696ad50f34814d6c78ecdcca2951ae43d2
Bryan Malinao <bnmalinao@up.edu.ph>
2025-10-20 01:15:20 +0000

    first commit

--- github.php
+++ github.php
@@ -4,7 +4,7 @@ define('GITHUB_SECRET', trim(@file_get_contents('/var/www/github.secret')));
 $header = 'HTTP_X_HUB_SIGNATURE';
 $secret = GITHUB_SECRET;
 $payload = file_get_contents('php://input');
-$hash = 'sha1='.hash_hmac('sha1', $payload, $secret);
+$hash = 'sha1=' . hash_hmac('sha1', $payload, $secret);
 
 if (isset($_SERVER[$header]) && $_SERVER[$header] === $hash) {
     $payload_decoded = json_decode($payload, true);
@@ -13,4 +13,3 @@ if (isset($_SERVER[$header]) && $_SERVER[$header] === $hash) {
     }
 }
 ?>
-
